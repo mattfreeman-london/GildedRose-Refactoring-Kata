b06a40022e364150b2cb59de156cf8fb7f7fba0c
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2019-12-19 10:49:51 +0000

    tests: does not change name, increases quality of aged brie, no quality change to Sulfuras

---
+++ ruby/spec/gilded_rose_spec.rb
@@ -0,0 +1,29 @@
+require 'gilded_rose'
+
+describe GildedRose do
+
+  describe "#update_quality" do
+    it "does not change the name" do
+      items = [Item.new("foo", 0, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].name).to eq "foo"
+    end
+
+    it 'increases the quality of Aged Brie over time' do
+      items = [Item.new("Aged Brie", 2, 0)]
+      GildedRose.new(items).update_quality()
+      GildedRose.new(items).update_quality()
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq(4)
+    end
+
+    it 'does not decrease the quality of Sulfuras' do
+      items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 80)]
+      GildedRose.new(items).update_quality()
+      GildedRose.new(items).update_quality()
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq(80)
+    end
+  end
+
+end
