384d8cdc40411acfa5d81231bfa51f01afded305
Matt Freeman <mathew.s.l.freeman@gmail.com>
2020-06-22 15:35:15 +0100

    edit: formatted file differently for readability

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -9,6 +9,7 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
+      
       if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
         if item.quality > 0
           if item.name != 'Sulfuras, Hand of Ragnaros'
@@ -32,11 +33,14 @@ class GildedRose
           end
         end
       end
+      
       if item.name != 'Sulfuras, Hand of Ragnaros'
         item.sell_in = item.sell_in - 1
       end
+      
       if item.sell_in < 0
         if item.name != 'Aged Brie'
+          
           if item.name != 'Backstage passes to a TAFKAL80ETC concert'
             if item.quality > 0
               if item.name != 'Sulfuras, Hand of Ragnaros'
@@ -46,12 +50,15 @@ class GildedRose
           else
             item.quality = item.quality - item.quality
           end
+
         else
           if item.quality < 50
             item.quality = item.quality + 1
           end
         end
+
       end
+
     end
   end
 end
